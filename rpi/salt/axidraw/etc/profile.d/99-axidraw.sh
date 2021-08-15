00679859f48a965c912237b0a83704f54728948b
Perry Naseck <git@perrynaseck.com>
2021-08-15 11:27:11 -0400

    fix instructions text and console var logic

--- rpi/salt/axidraw/etc/profile.d/99-axidraw.sh
+++ rpi/salt/axidraw/etc/profile.d/99-axidraw.sh
@@ -5,14 +5,14 @@ if [[ -z ${AXIDRAW_CONSOLE+x} ]]; then
   AXIDRAW_CLI_COLOR_NONE='\033[0m'
 
   echo -e "${AXIDRAW_CLI_COLOR_CYAN}Welcome to ${USER} on ${HOSTNAME}${AXIDRAW_CLI_COLOR_NONE}
-" 
-  
+"
+
   if getent group axidraw | cut -d: -f4- | grep -q "\b${USER}\b"; then
-       echo -e "Use the axicli command to control ${USER}. ${AXIDRAW_CLI_COLOR_RED}Do not use the -p, -P, --port, or --port_config opti$
-"  
-      alias axicli='/opt/venv-axidraw/bin/axicli --port `/usr/bin/readlink -f /dev/$USER` --port_config 0'
+    echo -e "Use the ${AXIDRAW_CLI_COLOR_CYAN}axicli${AXIDRAW_CLI_COLOR_NONE} command to control ${USER}. ${AXIDRAW_CLI_COLOR_RED}Do not use the -p, -P, --port, or --port_config options; this terminal will select the correct machine for you.${AXIDRAW_CLI_COLOR_NONE}
+"
+    alias axicli='/opt/venv-axidraw/bin/axicli --port `/usr/bin/readlink -f /dev/$USER` --port_config 0'
+    AXIDRAW_CONSOLE=true
+  else
+    AXIDRAW_CONSOLE=false
   fi
-  AXIDRAW_CONSOLE=true
-else
-  AXIDRAW_CONSOLE=false
 fi
